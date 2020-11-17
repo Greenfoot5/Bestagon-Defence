76a8af6a5d66b7adfe76ad96f8e1bd86c0a08073
Greenfoot5 <greenfoot89@gmail.com>
2020-11-17 06:08:51 +0000

    Added WaveSpawning.

---
+++ Tower Defence/Assets/WaveSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public float timeBetweenWaves = 5f;
+    private float _countdown = 2f;
+
+    private int _waveIndex;
+
+    public Transform spawnPoint;
+    
+    [SerializeField]
+    private float timeBetweenEnemies = 0.5f;
+
+    private void Update()
+    {
+        if (_countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            _countdown = timeBetweenWaves;
+        }
+
+        _countdown -= Time.deltaTime;
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        _waveIndex++;
+        
+        for (var i = 0; i < _waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
