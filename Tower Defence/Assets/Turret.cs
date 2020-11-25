cd763ef98774bf72d93fd9def20400c1ade3838d
Greenfoot5 <greenfoot89@gmail.com>
2020-11-25 00:46:27 +0000

    Added enemy searching and a turret script

---
+++ Tower Defence/Assets/Turret.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Transform target;
+    public float range = 2.5f;
+
+    public string enemyTag = "enemy";
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (var enemy in enemies)
+        {
+            float distanceToEnemey = Vector3.Distance(transform.position, enemy.transform.position);
+            
+            if (distanceToEnemey < shortestDistance)
+            {
+                shortestDistance = distanceToEnemey;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
