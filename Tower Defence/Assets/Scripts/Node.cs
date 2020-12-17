c72dacb951a11d3e3744e8bb89e6b9fafb62f631
Greenfoot5 <greenfoot89@gmail.com>
2020-12-17 15:16:50 +0000

    Nodes now change colour on hover, and you can click them

---
+++ Tower Defence/Assets/Scripts/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColour;
+    private Color _defaultColour;
+
+    private GameObject _turret;
+
+    private Renderer _rend;
+
+    void Start()
+    {
+        _rend = GetComponent<Renderer>();
+        _defaultColour = _rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        // TODO - Enable turret upgrades
+        if (_turret != null)
+        {
+            Debug.Log("Turret already built!");
+        }
+        
+        // Build a turret
+    }
+
+    private void OnMouseEnter()
+    {
+        _rend.material.color = hoverColour;
+    }
+
+    private void OnMouseExit()
+    {
+        _rend.material.color = _defaultColour;
+    }
+}
