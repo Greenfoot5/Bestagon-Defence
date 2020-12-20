5d2a6c202ecc69730378b80f857ef86eb8bdde08
Greenfoot5 <greenfoot89@gmail.com>
2020-12-20 21:07:21 +0000

    Added basic panning

---
+++ Tower Defence/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 10f;
+    [Range(0,1)]
+    [Tooltip("Percentage of the screen from border to start panning")]
+    public float panBorderPercentage = 0.95f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height * panBorderPercentage)
+        {
+            transform.Translate(Vector3.up * (panSpeed * Time.deltaTime), Space.World);
+        }
+        else if (Input.GetKey("s") || Input.mousePosition.y <= Screen.height * (1 - panBorderPercentage))
+        {
+            transform.Translate(Vector3.down * (panSpeed * Time.deltaTime), Space.World);
+        }
+        else if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width * panBorderPercentage)
+        {
+            transform.Translate(Vector3.right * (panSpeed * Time.deltaTime), Space.World);
+        }
+        else if (Input.GetKey("a") || Input.mousePosition.x <= Screen.width * (1 - panBorderPercentage))
+        {
+            transform.Translate(Vector3.left * (panSpeed * Time.deltaTime), Space.World);
+        }
+    }
+}
