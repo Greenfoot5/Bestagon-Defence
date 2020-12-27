a623bc51689eaa0ee13650bdbd307250d6c3bd20
Greenfoot5 <greenfoot89@gmail.com>
2020-12-27 23:11:29 +0000

    Added a shop script

---
+++ Tower Defence/Assets/Scripts/Shop.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseStandardTurret()
+    {
+        Debug.Log("Standard Turret Purchased.");
+    }
+
+    public void PurchaseAnotherTurret()
+    {
+        
+    }
+}
