c706149c6207a9d9fe135635148c91205c3ffe2f
Greenfoot5 <greenfoot89@gmail.com>
2021-01-04 17:06:08 +0000

    Now costs money to build a turret

--- Tower Defence/Assets/Scripts/BuildManager.cs
+++ Tower Defence/Assets/Scripts/BuildManager.cs
@@ -21,8 +21,18 @@ public class BuildManager : MonoBehaviour
     // Called when we build
     public void BuildTurretOn(Node node)
     {
+        if (GameStats.gold < _turretToBuild.cost)
+        {
+            Debug.Log("Not enough gold!");
+            return;
+        }
+
+        GameStats.gold -= _turretToBuild.cost;
+        
         var turret = Instantiate(_turretToBuild.prefab, node.transform.position, Quaternion.identity);
         node.turret = turret;
+        
+        Debug.Log("Turret build. Gold left: " + GameStats.gold);
     }
     
     // Used to set the turret we want to build.
---
+++ Tower Defence/Assets/Scripts/GameStats.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GameStats : MonoBehaviour
+{
+    public static int gold;
+    public int startGold = 200;
+
+    void Start()
+    {
+        gold = startGold;
+    }
+}
