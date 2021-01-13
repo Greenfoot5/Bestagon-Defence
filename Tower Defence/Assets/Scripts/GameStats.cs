e228955ecd6f83819d05067b2f553d7f93dcd66a
Greenfoot5 <greenfoot89@gmail.com>
2021-01-13 19:32:04 +0000

    Added a lives counter.

--- Tower Defence/Assets/Scripts/GameStats.cs
+++ Tower Defence/Assets/Scripts/GameStats.cs
@@ -5,8 +5,12 @@ public class GameStats : MonoBehaviour
     public static int money;
     public int startMoney = 200;
 
+    public static int lives;
+    public int startLives = 20;
+
     void Start()
     {
         money = startMoney;
+        lives = startLives;
     }
 }
---
+++ Tower Defence/Assets/Scripts/LivesUI.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class LivesUI : MonoBehaviour
+{
+    // The money counter
+    public TMP_Text livesText;
+
+    void Update()
+    {
+        livesText.text = "<sprite=\"EmojiOne\" name=\"1f60d\">" + GameStats.lives;
+    }
+}
