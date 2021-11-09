c3231272ad69f1a1ea46d086d2f6180c60665ff4
Greenfoot5 <greenfoot89@gmail.com>
2021-11-09 00:54:53 +0000

    Updated TurretSelectionUI

--- Tower Defence/Assets/Scripts/UI/TurretSelectionUI.cs
+++ Tower Defence/Assets/Scripts/UI/TurretSelectionUI.cs
@@ -1,100 +1,84 @@
-using System;
 using TMPro;
+using Turrets;
 using Turrets.Blueprints;
 using UnityEngine;
 using UnityEngine.UI;
-using Turrets;
 
-public class TurretSelectionUI : MonoBehaviour
+namespace UI
 {
-    private TurretBlueprint _turretBlueprint;
-    
-    // Content
-    public TextMeshProUGUI displayName;
-    public TextMeshProUGUI tagline;
-
-    public Image icon;
-    
-    [Header("Upgrades")]
-    public GameObject noneText;
-    public GameObject upgradesLayout;
-    public GameObject upgradeUI;
-
-    [Header("Stats")]
-    public TurretStat damage;
-    public TurretStat rate;
-    public TurretStat range;
-
-    [Header("Colors")]
-    public Hexagons bg;
-    public Image upgradesBG;
-    public TextMeshProUGUI upgradesTitle;
-
-    // Called when creating the UI
-    public void Init (TurretBlueprint turret, Shop shop)
+    public class TurretSelectionUI : MonoBehaviour
     {
-        _turretBlueprint = turret;
+        private TurretBlueprint _turretBlueprint;
+    
+        // Content
+        public TextMeshProUGUI displayName;
+        public TextMeshProUGUI tagline;
 
-        displayName.text = turret.displayName;
-        tagline.text = turret.tagline;
+        public Image icon;
+    
+        [Header("Upgrades")]
+        public GameObject noneText;
+        public GameObject upgradesLayout;
+        public GameObject upgradeUI;
 
-        icon.sprite = turret.shopIcon;
+        [Header("Stats")]
+        public TurretStat damage;
+        public TurretStat rate;
+        public TurretStat range;
 
-        var turretPrefab = turret.prefab.GetComponent<Turret>();
-        switch (turretPrefab.attackType)
+        [Header("Colors")]
+        public Hexagons bg;
+        public Image upgradesBG;
+        public TextMeshProUGUI upgradesTitle;
+
+        // Called when creating the UI
+        public void Init (TurretBlueprint turret, Shop shop)
         {
-            case TurretType.Bullet:
-                damage.SetData(
-                    turretPrefab.bulletPrefab.GetComponent<Bullet>().damage
-                    );
-                break;
+            _turretBlueprint = turret;
 
-            case TurretType.Laser:
-                damage.SetData(turretPrefab.damageOverTime);
-                break;
+            displayName.text = turret.displayName;
+            tagline.text = turret.tagline;
 
-            case TurretType.Area:
-                damage.SetData(turretPrefab.smashDamage);
-                break;
-            
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-        rate.SetData(turretPrefab.fireRate);
-        range.SetData(turretPrefab.range);
+            icon.sprite = turret.shopIcon;
 
-        if (turret.upgrades.Count == 0)
-        {
-            noneText.SetActive(true);
-        }
-        else
-        {
-            foreach (var upgrade in turret.upgrades)
+            var turretPrefab = turret.prefab.GetComponent<Turret>();
+            damage.SetData(turretPrefab.damage);
+            rate.SetData(turretPrefab.fireRate);
+            range.SetData(turretPrefab.range);
+
+            if (turret.upgrades.Count == 0)
             {
-                var up = Instantiate(upgradeUI, upgradesLayout.transform);
-                up.GetComponentInChildren<TurretUpgradesUpgrade>().SetData(upgrade);
+                noneText.SetActive(true);
             }
+            else
+            {
+                foreach (var upgrade in turret.upgrades)
+                {
+                    var up = Instantiate(upgradeUI, upgradesLayout.transform);
+                    up.GetComponentInChildren<TurretUpgradesUpgrade>().SetData(upgrade);
+                }
+            }
+
+            // Colors
+            tagline.color = turret.accent;
+            upgradesTitle.color = turret.accent;
+            bg.color = turret.accent;
+            upgradesBG.color = turret.accent * new Color(1, 1, 1, .16f);
+
+            damage.SetColor(turret.accent);
+            rate.SetColor(turret.accent);
+            range.SetColor(turret.accent);
+
+            bg.GetComponent<Button>().onClick.AddListener(delegate { MakeSelection(shop); });
         }
 
-        // Colors
-        tagline.color = turret.accent;
-        upgradesTitle.color = turret.accent;
-        bg.color = turret.accent;
-        upgradesBG.color = turret.accent * new Color(1, 1, 1, .16f);
-
-        damage.SetColor(turret.accent);
-        rate.SetColor(turret.accent);
-        range.SetColor(turret.accent);
-
-        bg.GetComponent<Button>().onClick.AddListener(delegate { MakeSelection(shop); });
-    }
-
-    // Called when the user clicks on the button
-    private void MakeSelection (Shop shop)
-    {
-        transform.parent.gameObject.SetActive (false);
-        Time.timeScale = 1f;
+        // Called when the user clicks on the button
+        private void MakeSelection (Shop shop)
+        {
+            transform.parent.gameObject.SetActive (false);
+            Time.timeScale = 1f;
         
-        shop.SpawnNewTurret(_turretBlueprint);
+            shop.SpawnNewTurret(_turretBlueprint);
+        }
     }
 }
