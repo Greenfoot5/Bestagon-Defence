87724d5a498c35f7a2cdd36c591b25d13a6c277e
Greenfoot5 <greenfoot89@gmail.com>
2021-12-10 23:35:25 +0000

    Cleaned up CameraController with #defines and docstrings

--- Tower Defence/Assets/Scripts/CameraController.cs
+++ Tower Defence/Assets/Scripts/CameraController.cs
@@ -2,10 +2,15 @@
 
 public class CameraController : MonoBehaviour
 {
+    #if UNITY_STANDALONE
     [Tooltip("Keyboard panning multiplier")]
     public float keyboardPanSpeed = 10f;
+    
+    #elif UNITY_ANDROID || UNITY_IOS
     [Tooltip("Swipe panning multiplier")]
     public float swipePanSpeed = 1f;
+    #endif
+    
     [Tooltip("The minimum position the camera can reach")]
     public Vector2 minPos = new Vector2(0, 0);
     [Tooltip("The maximum position the camera can reach")]
@@ -16,7 +21,7 @@ public class CameraController : MonoBehaviour
     public float scrollSpeed = 5000f;
     [Tooltip("The minimum Orthographic size that can be reached (maximum zoom)")]
     public float minOrthSize = 3;
-    [Tooltip("The maximun Orthographic size that can be reached (minimum zoom)")]
+    [Tooltip("The maximum Orthographic size that can be reached (minimum zoom)")]
     public float maxOrthSize = 9;
     private Camera _camera;
 
@@ -25,40 +30,50 @@ public class CameraController : MonoBehaviour
 
     private float _prevPinchMag;
 
-    void Start()
+    private void Start()
     {
         _camera = transform.GetComponent<Camera>();
     }
     
-    void Move()
+    /// <summary>
+    /// Called every update. Used when the player wants to move the camera
+    /// </summary>
+    private void Move()
     {
+        #if UNITY_STANDALONE
         // Keyboard & Mouse Input
         _cameraSpeed = new Vector2(Input.GetAxis("Pan Horizontal"), Input.GetAxis("Pan Vertical"));
         _cameraSpeed *= keyboardPanSpeed;
         
+        
+        #elif UNITY_ANDROID || UNITY_IOS
         // Mobile Input
-        if (Input.touches.Length == 1)
+        if (Input.touches.Length != 1) return;
+        var touch = Input.touches[0];
+        if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
         {
-            Touch touch = Input.touches[0];
-            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
-            {
-                _cameraSpeed = -touch.deltaPosition * swipePanSpeed;
-            }
+            _cameraSpeed = -touch.deltaPosition * swipePanSpeed;
         }
+        #endif
     }
-
-    void Scroll()
+    
+    /// <summary>
+    /// Called every update. Used when the player wants the camera to scroll in or out
+    /// </summary>
+    private void Scroll()
     {
+        #if UNITY_STANDALONE
         // Keyboard & Mouse input
         _scrolling = Input.mouseScrollDelta.y * scrollSpeed;
         
+        #elif UNITY_ANDROID || UNITY_IOS
         // Mobile input
         if (Input.touches.Length == 2)
         {
-            Vector2 touch0Pos = Input.touches[0].position;
-            Vector2 touch1Pos = Input.touches[1].position;
+            var touch0Pos = Input.touches[0].position;
+            var touch1Pos = Input.touches[1].position;
 
-            Vector2 pinchLength = touch0Pos - touch1Pos;
+            var pinchLength = touch0Pos - touch1Pos;
 
             if (_prevPinchMag != 0)
             {
@@ -71,9 +86,10 @@ public class CameraController : MonoBehaviour
         {
             _prevPinchMag = 0f;
         }
+        #endif
     }
-    
-    void Update()
+
+    private void Update()
     {
         // Disable panning if the game is over
         if (GameManager.isGameOver)
@@ -88,14 +104,14 @@ public class CameraController : MonoBehaviour
         
         // Move's the camera
         var transformPosition = transform.position;
-        float newPositionX = Mathf.Clamp(transformPosition.x + _cameraSpeed.x * Time.deltaTime, minPos.x, maxPos.x);
-        float newPositionY = Mathf.Clamp(transformPosition.y + _cameraSpeed.y * Time.deltaTime, minPos.y, maxPos.y);
+        var newPositionX = Mathf.Clamp(transformPosition.x + _cameraSpeed.x * Time.deltaTime, minPos.x, maxPos.x);
+        var newPositionY = Mathf.Clamp(transformPosition.y + _cameraSpeed.y * Time.deltaTime, minPos.y, maxPos.y);
         transform.Translate(new Vector3(newPositionX, newPositionY, transformPosition.z) - transformPosition, Space.World);
         // Reset the camera's speed, in case the user stops moving
         _cameraSpeed = new Vector2();
 
         // Implement scrolling by changing the Orthographic Size on the camera
-        float orthSize = _camera.orthographicSize;
+        var orthSize = _camera.orthographicSize;
         orthSize -= _scrolling * Time.deltaTime;
         orthSize = Mathf.Clamp(orthSize, minOrthSize, maxOrthSize);
 
