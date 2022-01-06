fa9dc418ba280597d64b81fffa15793282325914
Greenfoot5 <greenfoot89@gmail.com>
2022-01-06 23:14:38 +0000

    Stats can have negative stats, but they will behave as 0

--- Tower Defence/Assets/Scripts/UpgradableStat.cs
+++ Tower Defence/Assets/Scripts/UpgradableStat.cs
@@ -11,15 +11,26 @@ public struct UpgradableStat
         stat = baseValue;
         modifier = 1f;
     }
-
+    
+    /// <summary>
+    /// Gets the base value of the stat.
+    /// If the value is less than 0, it will return 0
+    /// </summary>
+    /// <returns>The base value of the stat</returns>
     public float GetBase()
     {
-        return stat;
+        return stat <= 0f ? 0f : stat;
     }
-
+    
+    
+    /// <summary>
+    /// Gets the stat after it's been modified
+    /// If the value is less than 0, it will return 0
+    /// </summary>
+    /// <returns>The stat after being multiplied by the modifier</returns>
     public float GetStat()
     {
-        return stat * modifier;
+        return stat * modifier <= 0f ? 0f : stat * modifier;
     }
 
     public void AddModifier(float newValue)
