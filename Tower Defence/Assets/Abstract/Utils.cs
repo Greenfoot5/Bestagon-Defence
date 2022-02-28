469b1a36672fe09ace7e484a37ffd57038011495
BLOODWIING <bandym555@gmail.com>
2022-02-28 20:13:24 +0200

    Moved a string function to Utils

---
+++ Tower Defence/Assets/Abstract/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// Adds spaces before capital letters to a string.
+        /// </summary>
+        /// <param name="text">String to space</param>
+        /// <returns>Spaced <see cref="string"/></returns>
+        public static string AddSpacesToSentence(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return "";
+            var newText = new StringBuilder(text.Length * 2);
+            newText.Append(text[0]);
+            for (var i = 1; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]) && text[i - 1] != ' ')
+                    newText.Append(' ');
+                newText.Append(text[i]);
+            }
+            return newText.ToString();
+        }
+    }
+}
