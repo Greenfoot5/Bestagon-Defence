806f592b6899a80f7f1c7ecffdbe385aa4db7ff0
Greenfoot5 <greenfoot89@gmail.com>
2022-08-27 14:54:56 +0100

    Updated Level Select

--- Tower Defence/Assets/UI/RandomHueOnLoad.cs
+++ Tower Defence/Assets/UI/RandomHueOnLoad.cs
@@ -6,19 +6,20 @@ namespace UI
     public class RandomHueOnLoad : MonoBehaviour
     {
         [SerializeField]
-        private TMP_Text text;
+        private TMP_Text[] texts;
 
         private void Start()
         {
-            // Load the old colours
-            float h;
-            float s;
-            float v;
-            Color.RGBToHSV(text.color, out h, out s, out v);
-        
-            // Randomise the hue and assign
-            h = Random.value;
-            text.color = Color.HSVToRGB(h, s, v);
+            float hue = Random.value;
+            foreach (TMP_Text text in texts)
+            {
+                // Load the old colours
+                Color.RGBToHSV(text.color, out float h, out float s, out float v);
+
+                // Randomise the hue and assign
+                h = hue;
+                text.color = Color.HSVToRGB(h, s, v);
+            }
         }
     }
 }
