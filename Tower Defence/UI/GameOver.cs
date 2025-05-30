d3b18af05ec7b4f13c8b6e9b523f3f3a269ebe31
Greenfoot5 <greenfoot89@gmail.com>
2025-05-30 19:47:17 +0100

    Actually remove VisibilityChanged listener from GameOver

--- Tower Defence/UI/GameOver.cs
+++ Tower Defence/UI/GameOver.cs
@@ -22,7 +22,7 @@ namespace UI
 
         public override void _ExitTree()
         {
-            VisibilityChanged += OnEnable;
+            VisibilityChanged -= OnEnable;
         }
         
         /// <summary>
