294c994265a83d430bb6a6b87d1e3f5fae238a25
Greenfoot5 <greenfoot89@gmail.com>
2025-06-06 12:47:00 +0100

    Implememented Laser

--- Tower Defence/Turrets/Laser/Laser.cs
+++ Tower Defence/Turrets/Laser/Laser.cs
@@ -1,4 +1,6 @@
-﻿using Abstract.Attributes;
+﻿using System.Collections.Generic;
+using Abstract.Attributes;
+using Enemies;
 using Godot;
 
 namespace Turrets.Laser
@@ -6,9 +8,10 @@ namespace Turrets.Laser
     /// <summary>
     /// Extends DynamicTurret to add Laser functionality
     /// </summary>
-    // TODO - Make work
     public partial class Laser : DynamicTurret
     {
+        [Export]
+        private Line2D line;
         // Lasers
         // <summary>
         // The line renderer that displays the laser
@@ -22,9 +25,9 @@ namespace Turrets.Laser
         // private ParticleSystem impactEffect;
 
         /// <summary> How long left until the next attack </summary>
-        public double durationCountdown;
+        public double DurationCountdown;
         /// <summary> How long left until the next attack </summary>
-        public double cooldownCountdown;
+        public double CooldownCountdown;
 
         public Laser()
         {
@@ -34,22 +37,20 @@ namespace Turrets.Laser
 
         /// <summary>
         /// Fires the laser if the turret have a target and are looking at them.
-        /// Otherwise rotate to target if there is one.
+        /// Otherwise, rotate to target if there is one.
         /// </summary>
         public override void _PhysicsProcess(double delta)
         {
-            durationCountdown -= delta;
+            DurationCountdown -= delta;
             
-            if (cooldownCountdown >= 0 && durationCountdown <= 0)
+            if (CooldownCountdown >= 0 && DurationCountdown <= 0)
             {
-                cooldownCountdown -= delta;
+                CooldownCountdown -= delta;
 
                 if (TargetEnemy is not null)
                     LookAtTarget(delta);
                 
-                // if (!lineRenderer.enabled) return;
-                
-                // lineRenderer.enabled = false;
+                line.Visible = false;
                 // impactEffect.Stop();
                 return;
             }
@@ -65,18 +66,18 @@ namespace Turrets.Laser
             LookAtTarget(delta);
             
             // One of the two laser timers expired
-            if (durationCountdown <= 0)
+            if (DurationCountdown <= 0)
             {
                 if (IsLookingAtTarget())
                 {
-                    durationCountdown = Stats[AttributeType.LaserDuration].Value;
-                    cooldownCountdown = Stats[AttributeType.LaserCooldown].Value;
+                    DurationCountdown = Stats[AttributeType.LaserDuration].Value;
+                    CooldownCountdown = Stats[AttributeType.LaserCooldown].Value;
                 }
 
                 return;
             }
             
-            Attack();
+            Attack((float)delta);
         }
         
         /// <summary>
@@ -85,45 +86,48 @@ namespace Turrets.Laser
         // TODO - Animate the laser slightly (make it pulse)
         protected override void Attack(float delta)
         {
-            Vector2 triangleCentre = Position;
-            
-            // Get the end point of the line renderer
-            // Vector2 direction = (firePoint.up * range.GetStat());
-            // Vector2 endPosition = (direction + triangleCentre);
-            
             // Get all enemies the laser hits
             // var results = new List<Collider2D>();
             // Physics2D.OverlapCapsule(direction/2 + triangleCentre, new Vector2(lineRenderer.endWidth, range.GetStat()), 
                 // CapsuleDirection2D.Vertical, transform.Rotation.Y, new ContactFilter2D().NoFilter(), results);
             // List<Enemy> enemies = results.Select(result => result.transform.GetComponent<Enemy>()).ToList();
             // enemies.RemoveAll(x => x == null);
+
+            var enemies = new List<Enemy>();
+
+            while (Ray.GetCollider() != null)
+            {
+                Ray.AddException((CollisionObject2D)Ray.GetCollider());
+                if (Ray.GetCollider() is Enemy enemy)
+                {
+                    Ray.ForceRaycastUpdate();
+                    enemies.Add(enemy);
+                }
+            }
+            
+            Ray.ClearExceptions();
             
             base.Attack(this);
-            // HitMany(enemies, this);
+            HitMany(enemies, this);
             
             // Deal damage to every enemy hit
-            // foreach (Enemy enemy in enemies)
-            // {
-                // enemy.TakeDamage(damage.GetStat() * delta, this);
-            // }
+            foreach (Enemy enemy in enemies)
+            {
+                enemy.TakeDamage(Stats[AttributeType.Damage].Value * delta, this);
+            }
 
             // Enable visuals
-            // if (!lineRenderer.enabled)
-            // {
-                // lineRenderer.enabled = true;
+            if (!line.Visible)
+            {
+                line.Visible = true;
                 // impactEffect.Play();
-            // }
-            
-            // Set Laser positions
-            Vector2 firePointPosition = Position;
-            // lineRenderer.SetPosition(0, firePointPosition);
-            // lineRenderer.SetPosition(1, endPosition);
-            
+            }
+
             // Set impact effect rotation
             // Transform impactEffectTransform = impactEffect.transform;
             // var aimDir = (Vector2)((Vector2)firePointPosition - (Vector2)impactEffectTransform.position).normalized;
             // impactEffectTransform.Rotation = Quaternion.LookRotation(aimDir);
-            
+
             // Set impact effect position
             //impactEffectTransform.position = endPosition + aimDir * 0.2f;
         }
